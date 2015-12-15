22e7059b1cf867f9d3351618bcc1b6a583b89a98
Zhen Li <zhen@cs.toronto.edu>
2015-12-15 12:39:09 -0500

    Calculate precision recall for car vs all and pes vs all

--- runSVM.m
+++ runSVM.m
@@ -12,14 +12,17 @@ pedNum = length(dir(fullfile(pedDir, '*.png')));
 pedDir1 = 'crop/pedestrian_1';
 pedNum1 = length(dir(fullfile(pedDir1, '*.png')));
 
-fprintf('Found %d cars, %d pedestrians.\n', carNum, pedNum+pedNum1);
+bgDir = 'crop/background';
+bgNum = length(dir(fullfile(bgDir, '*.png')));
 
-%% Load images. 35sec.
+fprintf('Found %d cars, %d pedestrians, %d background images.\n', carNum, pedNum+pedNum1, bgNum);
+
+%% Load images. 95 sec.
 nSize = 64;
+tic;
 
 carImg = zeros(nSize, nSize, carNum);
 counter = 0;
-tic;
 for i = 1:carNum
     img = rgb2gray(imread(sprintf('%s/%06d.png', carDir, i-1)));
     if size(img, 1) > 35
@@ -50,9 +53,17 @@ end
 pedNum = counter;
 pedImg = pedImg(:,:, 1:pedNum);
 fprintf('    %d selected pedestrians.\n', pedNum);
+
+bgImg = zeros(nSize, nSize, bgNum);
+for i = 1:bgNum
+    img = rgb2gray(imread(sprintf('%s/%06d.png', bgDir, i-1)));
+    bgImg(:,:,i) = imresize(img, [nSize nSize]);
+end
+fprintf('    %d background images.\n', bgNum);
+
 fprintf('Load Images: %f seconds\n', toc);
 
-%% Extract HoG for training & testing set. 40 sec.
+%% Extract HoG for training & testing set. 30 sec.
 tic;
 cellSize = [4 4];
 % img = rgb2gray(imread(sprintf('%s/%06d.png', carDir, 0)));
@@ -70,21 +81,34 @@ rng(1);
 % carNum = pedNum;
 randCarIndex = randperm(carNum);
 randPedIndex = randperm(pedNum);
+randBgIndex = randperm(bgNum);
 
 trainCarNum = round(carNum * 0.7);
 trainPedNum = round(pedNum * 0.7);
-fprintf('Train&Validate on %d/%d cars, %d/%d pedestrians.\n', ...
-    trainCarNum, carNum, trainPedNum, pedNum);
+trainBgNum = round(bgNum * 0.7);
+testCarNum = carNum - trainCarNum;
+testPedNum = pedNum - trainPedNum;
+testBgNum = bgNum - trainBgNum;
 
-trainNum = trainCarNum+trainPedNum;
+fprintf('Train&Validate on %d/%d cars, %d/%d pedestrians, %d/%d background images.\n', ...
+    trainCarNum, carNum, trainPedNum, pedNum, trainBgNum, bgNum);
+
+% trainNum = trainCarNum+trainPedNum;
+trainNum = trainCarNum + trainPedNum + trainBgNum;
 trainFeatures = zeros(trainNum, hogFeatureSize, 'single');
-trainLabels = zeros(trainNum, 1, 'uint8');
-trainLabels(1:trainCarNum, 1) = 1;
+trainLabelsCar = zeros(trainNum, 1, 'uint8');
+trainLabelsCar(1 : trainCarNum, 1) = 1;
+trainLabelsPed = zeros(trainNum, 1, 'uint8');
+trainLabelsPed(trainCarNum+1 : trainCarNum+trainPedNum, 1) = 1;
 
-testNum = carNum + pedNum - trainNum;
+% testNum = carNum + pedNum - trainNum;
+testNum = testCarNum + testPedNum + testBgNum;
+
 testFeatures = zeros(testNum, hogFeatureSize, 'single');
-testLabels = zeros(testNum, 1, 'uint8');
-testLabels(1 : carNum-trainCarNum, 1) = 1;
+testLabelsCar = zeros(testNum, 1, 'uint8');
+testLabelsCar(1 : testCarNum, 1) = 1;
+testLabelsPed = zeros(testNum, 1, 'uint8');
+testLabelsPed(testCarNum+1 : testCarNum+testPedNum, 1) = 1;
 
 for i=1:trainCarNum
     trainFeatures(i, :) = extractHOGFeatures(carImg(:,:,randCarIndex(i)), 'CellSize', cellSize);
@@ -93,30 +117,67 @@ for j=1:trainPedNum
     i = j + trainCarNum;
     trainFeatures(i, :) = extractHOGFeatures(pedImg(:,:,randPedIndex(j)), 'CellSize', cellSize);
 end
+for k=1:trainBgNum
+    i = k + trainCarNum + trainPedNum;
+    trainFeatures(i, :) = extractHOGFeatures(bgImg(:,:,randBgIndex(k)), 'CellSize', cellSize);
+end
 
 for i=trainCarNum+1 : carNum
     testFeatures(i - trainCarNum, :) = extractHOGFeatures(carImg(:,:,randCarIndex(i)), 'CellSize', cellSize);
 end
-dj = carNum - trainCarNum - trainPedNum;
+dj = testCarNum - trainPedNum;
 for j=trainPedNum+1 : pedNum
     i = j + dj;
     testFeatures(i, :) = extractHOGFeatures(pedImg(:,:,randPedIndex(j)), 'CellSize', cellSize);
 end
+dk = testCarNum + testPedNum - trainBgNum;
+for k=trainBgNum+1 : bgNum
+    i = k + dk;
+    testFeatures(i, :) = extractHOGFeatures(bgImg(:,:,randBgIndex(k)), 'CellSize', cellSize);
+end
+
 fprintf('Extract HoG Features: %f seconds\n', toc);
-%% Train a classifier. 18 + 117 + 15 sec.
+%% Train two OvA (One vs All) classifiers. 161 + 1053 + 48 sec./ 103 + 721 + 32sec.
+% Car vs all
+fprintf('Preparing SVM: Car vs all.\n');
 tic;
-SVMModel = fitcsvm(trainFeatures, trainLabels);
-fprintf('Train SVM: %f seconds\n', toc);
+SVMModelCar = fitcsvm(trainFeatures, trainLabelsCar);
+fprintf('    Train SVM: %f seconds\n', toc);
 
 tic;
-CVSVMModel = crossval(SVMModel, 'KFold', 7);
+CVSVMModel = crossval(SVMModelCar, 'KFold', 7);
 classLoss = kfoldLoss(CVSVMModel)
-save('svm.mat', 'SVMModel');
-fprintf('Cross Validation SVM: %f seconds\n', toc);
+save('svm_car.mat', 'SVMModelCar');
+fprintf('    Cross Validation SVM: %f seconds\n', toc);
 
 tic;
-[predictLabel, score] = predict(SVMModel, testFeatures);
-errorRes = find(predictLabel ~= testLabels);
-errorRate = numel(errorRes) / size(predictLabel, 1);
-fprintf('Error rate:%f\n', errorRate);
-fprintf('Test SVM: %f seconds\n', toc);
+[predictLabel, score] = predict(SVMModelCar, testFeatures);
+corrVec = (predictLabel == testLabelsCar);
+corrRate = sum(corrVec) / size(predictLabel, 1);
+TP = sum(corrVec(1:testCarNum));
+precision = TP / sum(predictLabel);
+recall = TP / testCarNum;
+fprintf('    Correct rate:%f, Precision:%f, Recall:%f\n', corrRate, precision, recall);
+fprintf('    Test SVM: %f seconds\n', toc);
+
+% Pedestrian vs all
+fprintf('Preparing SVM: Pedestrian vs all.\n');
+tic;
+SVMModelPed = fitcsvm(trainFeatures, trainLabelsPed);
+fprintf('    Train SVM: %f seconds\n', toc);
+
+tic;
+CVSVMModel = crossval(SVMModelPed, 'KFold', 7);
+classLoss = kfoldLoss(CVSVMModel)
+save('svm_ped.mat', 'SVMModelPed');
+fprintf('    Cross Validation SVM: %f seconds\n', toc);
+
+tic;
+[predictLabel, score] = predict(SVMModelPed, testFeatures);
+corrVec = (predictLabel == testLabelsPed);
+corrRate = sum(corrVec) / size(predictLabel, 1);
+TP = sum(corrVec(testCarNum+1 : testCarNum+testPedNum));
+precision = TP / sum(predictLabel);
+recall = TP / testPedNum;
+fprintf('    Correct rate:%f, Precision:%f, Recall:%f\n', corrRate, precision, recall);
+fprintf('    Test SVM: %f seconds\n', toc);
