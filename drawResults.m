279a105484e3c2a93ac92c1de2889417beee1cbc
Zhen Li <zhen@cs.toronto.edu>
2015-12-20 20:42:41 -0500

    Draw 2d box

--- drawResults.m
+++ drawResults.m
@@ -17,20 +17,27 @@ fprintf('Found %d images(labels).\n', labelNum);
 
 imageNum = 1;%For debug
 
-%% Start evaluation
-h = figure();
-outObjects = readOutputLabels(outputDir, 15);
-drawBox2D(h, object);
-%%
+%% Draw
+
+% Overlap threshold for cars and pedestrians. 0.7 and 0.5
+% Ref: http://www.cvlibs.net/datasets/kitti/eval_object.php
+carTh = 0.7;
+pedTh = 0.5;
+
+% Minium bounding box height that we consider. 
+% Ref: Readme file from development kit
+minObjHeight = 25;
+
 tic;
 fprintf('Start drawing.\n');
 
 % for i=1:imageNum
-for i=16:17
+for i=31:33
     if mod(i,100) == 0
         fprintf('%d\n', i);
     end
     
+    im = imread(sprintf('%s/%06d.png', imageDir, i-1));
     % Read labels
     corrObjects = readLabels(labelDir, i-1);
     corrObjNum = numel(corrObjects);
@@ -39,9 +46,12 @@ for i=16:17
     % Increase if we find this object
     findObjects = zeros(corrObjNum, 1);
     
+    outRect = [];
+    corrRect = [];
+    
     % For each output objects
     for iOut = 1:outObjNum
-        if strcmp(outObjects(iOut).type, 'Car')
+        if strcmp(outObjects(iOut).type, 'Car') && abs(outObjects(iOut).score) > 1
             found = false;
             % Check each correct cars and vans
             for iCorr = 1:corrObjNum
@@ -55,11 +65,10 @@ for i=16:17
                     end
                 end
             end
-            if ~found
-                % Claim a 'car' but actually not.
-                carFP = carFP + 1;
-            end
-        elseif strcmp(outObjects(iOut).type, 'Pedestrian')
+            % Draw a car:
+            outRect = [outRect; outObjects(iOut).x1, outObjects(iOut).y1, ...
+                outObjects(iOut).x2-outObjects(iOut).x1, outObjects(iOut).y2-outObjects(iOut).y1];
+        elseif strcmp(outObjects(iOut).type, 'Pedestrian') && abs(outObjects(iOut).score) > 1
             found = false;
             % Check each correct pedestrians and sitting persons
             for iCorr = 1:corrObjNum
@@ -73,47 +82,33 @@ for i=16:17
                     end
                 end
             end
-            if ~found
-                % Claim a 'pedestrian' but actually not.
-                pedFP = pedFP + 1;
-            end
-        else
-            fprintf('Error: wrong type name.\n');
+            % Draw a pedestrian:
+            outRect = [outRect; outObjects(iOut).x1, outObjects(iOut).y1, ...
+                outObjects(iOut).x2-outObjects(iOut).x1, outObjects(iOut).y2-outObjects(iOut).y1];
         end
     end
     
     % Accumulate TP and FN for this image
     for iCorr = 1:corrObjNum
-       if findObjects(iCorr) > 0
-           % TP:
-           if strcmp(corrObjects(iCorr).type, 'Car') || strcmp(corrObjects(iCorr).type, 'Van')
-               carTP = carTP + 1;
-           elseif strcmp(corrObjects(iCorr).type, 'Pedestrian') || strcmp(corrObjects(iCorr).type, 'Person_sitting')
-               pedTP = pedTP + 1;
-           end
-       else
-           % FN:
-           if strcmp(corrObjects(iCorr).type, 'Car')
-               carFN = carFN + 1;
-           elseif strcmp(corrObjects(iCorr).type, 'Pedestrian')
-               pedFN = pedFN + 1;
-           end
-       end
+        if corrObjects(iCorr).y2 - corrObjects(iCorr).y1 >= minObjHeight
+            if strcmp(corrObjects(iCorr).type, 'Car') || (strcmp(corrObjects(iCorr).type, 'Van') && findObjects(iCorr) > 0)
+                % Draw a correct car
+                corrRect = [corrRect; corrObjects(iCorr).x1, corrObjects(iCorr).y1, ...
+                    corrObjects(iCorr).x2-corrObjects(iCorr).x1, corrObjects(iCorr).y2-corrObjects(iCorr).y1];
+            elseif strcmp(corrObjects(iCorr).type, 'Pedestrian') || (strcmp(corrObjects(iCorr).type, 'Person_sitting') && findObjects(iCorr) > 0)
+                % Draw a correct pedestrian
+                corrRect = [corrRect; corrObjects(iCorr).x1, corrObjects(iCorr).y1, ...
+                    corrObjects(iCorr).x2-corrObjects(iCorr).x1, corrObjects(iCorr).y2-corrObjects(iCorr).y1];
+            end
+        end
     end
+    
+    shapeInserter = vision.ShapeInserter('Shape','Rectangles','BorderColor','Custom', 'CustomBorderColor', uint8([255 0 0]), 'LineWidth', 2); 
+    J = step(shapeInserter, im, int32(outRect));
+    shapeInserter2 = vision.ShapeInserter('Shape','Rectangles','BorderColor','Custom', 'CustomBorderColor', uint8([0 255 0]), 'LineWidth', 4); 
+    K = step(shapeInserter2, J, int32(corrRect));
+    currFig = imshow(K); 
+    waitfor(currFig);
 end
 fprintf('Finished. Used %f seconds.\n', toc);
 
-carPrecision = carTP / (carTP + carFP);
-carRecall = carTP / (carTP + carFN);
-pedPrecision = pedTP / (pedTP + pedFP);
-pedRecall = pedTP / (pedTP + pedFN);
-
-Result = sprintf('    & TP & FP & FN & Precision & Recall\\\\ \nCar & %d & %d & %d & %f & %f\nPed & %d & %d & %d & %f & %f\n', ...
-    carTP, carFP, carFN, carPrecision, carRecall, ...
-    pedTP, pedFP, pedFN, pedPrecision, pedRecall);
-fprintf(Result);
-
-% Save to file
-fid = fopen('results.txt','w');
-fprintf(fid, Result);
-fclose(fid);
